2ffaab63734afb1fca5766a33f53f7989f714a82
jbh2032 <jbh2032@naver.com>
2022-03-04 15:12:38 +0900

    비밀번호 저장

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require("mongoose");
+const config = require("./config/key")
 
+app.use(express.json()) //For JSON requests
+app.use(express.urlencoded({ extended: true }));
 
+const { User } = require("./models/User")
 
-mongoose.connect("mongodb+srv://jbh2032:309203cj@cluster0.k4cli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
-    useNewUrlParser: true, useUnifiedTopology: true,
-}).then(()=>console.log("성공!!")).catch(err=>console.log(err));
+mongoose.connect(config.mongoURL, {
+  useNewUrlParser: true, useUnifiedTopology: true,
+}).then(() => console.log("성공!!")).catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!!!')
 })
+
+app.post("/register", (req, res) => {
+
+  const user = new User(req.body);
+  
+  user.save((err, doc) => {
+    if (err) return res.json({ 성공: false, err })
+    return res.status(200).json({
+      성공: true
+    })
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- models/User.js
+++ models/User.js
@@ -32,4 +32,4 @@ const userSchema = mongoose.Schema({
 })
 
 const User = mongoose.model("User",userSchema)
-module.exports = {User}
+module.exports = { User }
